83535b7412aefb657d28c74eb3cbbfafca547dbf
obayed_70@yahoo.comh <obayed_70@yahoo.com>
2015-04-27 15:19:43 -0400

    webApiBackEnd

---
+++ WebApiBackEnd/Models/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApiBackEnd.Models
+{
+    public class EmployeeContext: DbContext
+    {
+        public EmployeeContext() : base("Employees") { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+    
+}
